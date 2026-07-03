6f758a91b1dad232dc9aac3043011b61ed871f63
EEEEEEEEEEEEX <1165740681@qq.com>
2026-07-03 10:37:31 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-// #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
-// #include <vulkan/vulkan_raii.hpp>
-// #else
-// import vulkan_hpp;
-// #endif
-
 #include <vulkan/vulkan.h>
 
 #define GLFW_INCLUDE_VULKAN
