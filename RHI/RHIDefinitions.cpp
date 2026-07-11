f8ce094448401092dfb807cc0ee01b8cfaad97e4
EEEEEEEEEEEEX <1165740681@qq.com>
2026-07-11 10:26:32 +0800

    1

---
+++ RHI/RHIDefinitions.cpp
@@ -0,0 +1,18 @@
+﻿#include "RHIDefinitions.hpp"
+
+#include <type_traits>
+
+namespace rhi {
+
+static_assert(sizeof(u8)  == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+static_assert(sizeof(i32) == 4);
+
+static_assert(std::is_trivially_copyable_v<RHIBuffer>);
+static_assert(std::is_trivially_copyable_v<RHITexture>);
+static_assert(std::is_same_v<std::underlying_type_t<RHIGraphicsAPI>, u8>);
+static_assert(std::is_same_v<std::underlying_type_t<RHIRenderFeature>, u64>);
+
+} // namespace rhi
